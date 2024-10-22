4d5ed9fec1f921d2df65ae21ecfa679326159168
dnlaylo <laylomarga@gmail.com>
2024-10-23 00:38:25 +0800

    Loops, check valid name and age, and if invalid show an error

--- user_info_checker.py
+++ user_info_checker.py
@@ -1,15 +1,27 @@
 # Array for storing all entries
 data_entries = {}
 
-# Ask the user to input name and age
-name = input("Please enter your name: ")
-age = int(input("Please enter your age: "))
+# Loop for continuous input
+while True:
+    # Loop for validation of inputs
+    while True:
+        try:
+            # Ask the user to input name and age
+            # Have a valid name and valid age
+            # Display error message if the input is not valid
 
-# Have a valid name and valid age
-# Display error message if the input is not valid
-    # if len(name) < 3 or not name[0].isupper() then error
-    # if age < 18 or age > 65 then error
-    
+            # Check if name is valid, if not, raise an error
+            name = input("Please enter your name: ")
+            if len(name) < 3 or not name[0].isupper():
+                raise
+            # Check if age is valid, if not, raise an error
+            age = int(input("Please enter your age: "))
+            if age < 18 or age > 65:
+                raise
+
+        except:
+            print("Error")
+
 # Ask the user if they want to input another entry
     # If “Yes”
         # ask the user again for name and age entries
