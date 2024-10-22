c83bcb8df0d68af65d01632a48b85e846ceb2b5a
dnlaylo <laylomarga@gmail.com>
2024-10-23 00:20:12 +0800

    Pseudocode

---
+++ user_info_checker.py
@@ -0,0 +1,10 @@
+# Ask the user to input name and age
+# Have a valid name and valid age
+# Display error message if the input is not valid
+# Store all the collected data into an array
+# Ask the user if they want to input another entry
+    # If “Yes”
+        # ask the user again for name and age entries
+    # If "No"
+        # display the name and age of the oldest person.
+        # stop the program
